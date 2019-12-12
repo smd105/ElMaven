8996b3de9cc292114d41a5ed0a50502af0293832
Saiful B. Khan <saiful.khan@elucidata.io>
2019-12-12 17:42:44 +0530

    Display probability value in peak table

--- src/gui/mzroll/tabledockwidget.cpp
+++ src/gui/mzroll/tabledockwidget.cpp
@@ -177,6 +177,7 @@ void TableDockWidget::setupPeakTable() {
     colNames << "Max Quality";
     colNames << "MS2 Score";
     colNames << "#MS2 Events";
+    colNames << "Probability"; // TODO: add this column conditionally
     colNames << "Rank";
   } else if (viewType == peakView) {
     vector<mzSample *> vsamples = _mainwindow->getVisibleSamples();
@@ -430,12 +431,13 @@ void TableDockWidget::addRow(shared_ptr<PeakGroup> group,
     item->setText(11, QString::number(group->maxQuality, 'f', 2));
     item->setText(12, QString::number(group->fragMatchScore.mergedScore, 'f', 2));
     item->setText(13, QString::number(group->ms2EventCount));
-    item->setText(14, QString::number(group->groupRank, 'e', 6));
+    item->setText(14, QString::number(group->predictionProbability, 'f', 3));
+    item->setText(15, QString::number(group->groupRank, 'e', 6));
 
     if (group->changeFoldRatio != 0) {
 
-      item->setText(15, QString::number(group->changeFoldRatio, 'f', 2));
-      item->setText(16, QString::number(group->changePValue, 'e', 4));
+      item->setText(16, QString::number(group->changeFoldRatio, 'f', 2));
+      item->setText(17, QString::number(group->changePValue, 'e', 4));
     }
 
     //Find maximum number of peaks
@@ -2691,6 +2693,7 @@ void ScatterplotTableDockWidget::setupPeakTable() {
     colNames << "Max Quality";
     colNames << "MS2 Score";
     colNames << "#MS2 Events";
+    colNames << "Probability"; // TODO: add this column conditionally
     colNames << "Rank";
 
     // add scatterplot table columns
